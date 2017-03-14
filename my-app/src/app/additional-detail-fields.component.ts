34cdd488de177167281ac7bb33f99ab7d1d85c00
Katarzyna Wicherska <kawi@gft.com>
2017-03-14 15:50:57 +0100

    bad mix, TODO: formArray used in proper way, finish form with displaying additional inputs when the sum of subpayments is less than totalAmount

--- my-app/src/app/additional-detail-fields.component.ts
+++ my-app/src/app/additional-detail-fields.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
     selector: 'additional-detail-fields',
@@ -25,14 +25,52 @@ export class AdditionalDetailFieldsComponent {
 
 class ItemsValidators {
 
-  static minQuantitySum(val: number) {
-    return (c: AbstractControl) => {
-      let sum = c.value
-        .map(item => item.quantity)
-        .reduce((acc, cur) => acc + cur, 0 );
-      if (sum < val) {
-        return { minSum: val }
-      }
-    })
-  }
+    static minQuantitySum(val: number) {
+        return (c: AbstractControl) => {
+            let sum = c.value
+                .map(item => item.quantity)
+                .reduce((acc, cur) => acc + cur, 0);
+            if (sum < val) {
+                return { minSum: val }
+            }
+        })
+    }
+}
+
+@Component({
+    selector: 'item-control',
+    template:
+    `
+<div class="form-group row" [formGroup]="item">
+  <div class="col-sm-6">
+    <label [attr.for]="'name'+index">Name</label>
+    <input type="text" class="form-control" [attr.id]="'name'+index" formControlName="name">
+  </div>
+  <div class="col-sm-5">
+    <label [attr.for]="'quantity'+index">Quantity</label>
+    <input type="number" class="form-control" [attr.id]="'quantity'+index" formControlName="quantity">
+  </div>
+  <div class="col-sm-1 py-1">
+    <button type="button" class="btn" (click)="removed.emit(index)">-</button>
+  </div>
+</div>
+`
+})
+export class ItemFormControlComponent {
+
+    @Input()
+    public index: number;
+
+    @Input()
+    public item: FormGroup;
+
+    @Output()
+    public removed: EventEmitter<number> = new EventEmitter<number>();
+
+    static buildItem(val: string) {
+        return new FormGroup({
+            name: new FormControl(val, Validators.required),
+            quantity: new FormControl(100)
+        })
+    }
 }
